081803b6eea81e72623e69865b527640cb850736
bdan <24520247@gm.uit.edu.vn>
2026-04-16 01:52:31 +0700

    feat: forgot password; fix: change password, role

--- src/main/java/com/healthcare/backend/controller/AccountController.java
+++ src/main/java/com/healthcare/backend/controller/AccountController.java
@@ -8,8 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
