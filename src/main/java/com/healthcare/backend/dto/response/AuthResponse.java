23502009d6d787ca1e559fba9c79987a7e7dc856
eltuzk <phanhien261106@gmail.com>
2026-04-20 13:04:53 +0700

    fix: add constructor AuthResponse

--- src/main/java/com/healthcare/backend/dto/response/AuthResponse.java
+++ src/main/java/com/healthcare/backend/dto/response/AuthResponse.java
@@ -1,20 +1,15 @@
 package com.healthcare.backend.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
-        String accessToken;
-        
-        public AuthResponse() {
-        }
+    private String accessToken;
 
-        public AuthResponse(String accessToken) {
-            this.accessToken = accessToken;
-        }
-
-        public String getAccessToken() {
-            return accessToken;
-        }
-
-        public void setAccessToken(String accessToken) {
-            this.accessToken = accessToken;
-        }
 }
